e273902c2d4f64f283f7fc37b6d28d497efc090d
Connor Homan <connorhoman14@gmail.com>
2019-06-22 14:36:53 -0700

    Updated data set to reflect auction values

--- react-client/src/components/Player.jsx
+++ react-client/src/components/Player.jsx
@@ -8,13 +8,15 @@ const Card = styled.div`
   width: 355px;
 `;
 const Name = styled.span`
-  font-weight: bold;
+  font-weight: 900;
   align-content: left;
   margin-left: 5px;
   font-size: 100%;
+  color: #525151;
 `;
 const Team = styled.span`
   float: right;
+  color: #525151;
 `;
 const ADP = styled.span`
   font-size: 12px;
@@ -32,17 +34,19 @@ const Bye = styled.span`
 const Wrapper = styled.div`
   padding: 10px;
 `;
-const Pick = styled.span`
+const AAV = styled.span`
+  color: #525151;
   float: left;
   font-family: 'Times New Roman', Times, serif;
-  font-weight: 900;
+  font-weight: 500;
+  font-size: 15px;
 `;
 
 class Player extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      background: ''
+      background: '',
     }
   }
   turnColor() {
@@ -64,6 +68,9 @@ class Player extends React.Component {
     if (this.props.player.position === 'PK') {
       this.setState({background: '#ffbef5'});
     }
+    if (this.props.player.name.length === 6) {
+      this.setState({background: 'black'});
+    }
   }
   componentDidMount() {
     this.turnColor();
@@ -84,11 +91,11 @@ class Player extends React.Component {
         {(provided) => (
             <Card onClick={this.onClick.bind(this)} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
               <Wrapper style={{backgroundColor: this.state.background}}>
-                <Pick>
-                  { this.props.player.pick }
-                </Pick>
+                <AAV>
+                  ${ this.state.AAV }
+                </AAV>
                 <ADP>
-                  { this.props.player.ADP } ({ this.props.player.STD })
+                  ({ this.props.player.ADP })
                 </ADP>
                 <Name>
                   { this.props.player.name }
